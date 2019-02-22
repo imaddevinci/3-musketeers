84a12cb11a25da3d92780df95700b508d5e024f7
imad <imad.al_moslli@devinci.fr>
2019-02-22 13:43:26 +0100

    Test

--- starwars/src/index.test.js
+++ starwars/src/index.test.js
@@ -7,21 +7,25 @@ describe('starwars-names', () => {
       expect(starWars.all).toHaveLength(starWarsNames.length)
     });
     test('should be an array of strings', () => {
-      throw new Error('test not yet defined... remove the throw and write your test here');
+      for (var i=0;i<starWarsNames.length;i++)
+	  {
+		  expect(typeof(starWars[i]).toBe('string'));
+	  }
     });
 
     test('should contain `Luke Skywalker`', () => {
-      throw new Error('test not yet defined... remove the throw and write your test here');
+     expect(starWarsNames).toContain('Luke Skywalker');
     });
 
     test('should not contain `Ben Quadinaros`', () => {
-      throw new Error('test not yet defined... remove the throw and write your test here');
+           expect(starWarsNames).not.toContain('Ben Quadinaros');
+
     });
   });
 
   describe('random', () => {
     test('should return a random item from the starWars.all', () => {
-      throw new Error('test not yet defined... remove the throw and write your test here');
+     expect(starWarsNames).toContain(starWars.random());
     });
 
     test('should return an array of random items if passed a number', () => {
